7a6a1ae0b3b156f52f17163527c1e0c0f8fe4f39
Margot <m.pourrey@laposte.net>
2023-01-10 13:16:14 +0100

    debut ex01

---
+++ ex01/phonebook.cpp
@@ -0,0 +1,14 @@
+#include "phonebook.hpp"
+
+int	main ()
+{
+	Contact contact;
+	contact.set_contact(0, "Margot", "Pourrey", "Gomar");
+	contact.print_contact();
+
+/* 	PhoneBook my_phonebook;
+	my_phonebook.set_contact(0, "Margot");
+
+	std::cout << my_phonebook.get_contact(0) << std::endl; */
+
+}
